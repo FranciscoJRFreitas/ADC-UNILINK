594eff203e7bbdd947a981ffd5f1b48686220583
Martin <m.magdalinchev@campus.fct.unl.pt>
2023-05-03 11:16:57 +0100

    Token added to remove command

--- Backend&API/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RemoveResource.java
+++ Backend&API/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RemoveResource.java
@@ -1,15 +1,17 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
 import com.google.cloud.datastore.*;
-import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
+import com.google.gson.Gson;
+import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
 import pt.unl.fct.di.apdc.firstwebapp.util.RemoveData;
 import pt.unl.fct.di.apdc.firstwebapp.util.UserRole;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.logging.Logger;
@@ -18,6 +20,7 @@ import java.util.logging.Logger;
 public class RemoveResource {
 
     private final Datastore datastore = DatastoreOptions.newBuilder().setProjectId("ai-60313").build().getService();
+    private final Gson g = new Gson();
     private static final Logger LOG = Logger.getLogger(RegisterResource.class.getName());
 
     public RemoveResource() {
@@ -26,13 +29,24 @@ public class RemoveResource {
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteUser(RemoveData data) {
+    public Response deleteUser(RemoveData data, @Context HttpHeaders headers) {
+
+        String authTokenHeader = headers.getHeaderString("Authorization");
+        String authToken = authTokenHeader.substring("Bearer".length()).trim();
+        AuthToken token = g.fromJson(authToken, AuthToken.class);
+
+        Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
+        Key tokenKey = datastore.newKeyFactory().addAncestor(PathElement.of("User", token.getUsername()))
+                .setKind("User Token").newKey(token.username);
+        Key targetUserKey = datastore.newKeyFactory().setKind("User").newKey(data.targetUsername);
+
 
         Transaction txn = datastore.newTransaction();
         try {
 
-            Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
             Entity user = txn.get(userKey);
+            Entity originalToken = txn.get(tokenKey);
+            Entity targetUser = txn.get(targetUserKey);
 
             if (user == null) {
                 txn.rollback();
@@ -45,17 +59,12 @@ public class RemoveResource {
             if (!storedPassword.equals(providedPassword))
                 return Response.status(Response.Status.UNAUTHORIZED).entity("Incorrect password for user: " + data.username).build();
 
-            String storedToken = user.getString("user_token");
-            long storedTokenExpiration = user.getLong("user_token_expiration");
-            if (!storedToken.equals(data.token) || System.currentTimeMillis() > storedTokenExpiration) {
+            if (!token.tokenID.equals(originalToken.getString("user_token_ID"))|| System.currentTimeMillis() > token.expirationDate) {
                 txn.rollback();
                 return Response.status(Response.Status.UNAUTHORIZED).entity("Session Expired.").build();
             }
 
             if (!data.targetUsername.isEmpty()) {
-
-                Key targetUserKey = datastore.newKeyFactory().setKind("User").newKey(data.targetUsername);
-                Entity targetUser = txn.get(targetUserKey);
 
                 if (targetUser == null) {
                     txn.rollback();
@@ -67,13 +76,13 @@ public class RemoveResource {
                 if (!canDelete(userRole, targetUserRole, data))
                     return Response.status(Response.Status.UNAUTHORIZED).entity("You do not have the required permissions for this action.").build();
 
-                txn.delete(targetUserKey);
+                txn.delete(targetUserKey, tokenKey);
                 txn.commit();
                 LOG.info("User deleted: " + data.targetUsername);
                 return Response.ok("{}").build();
             }
 
-            txn.delete(userKey);
+            txn.delete(userKey, tokenKey);
             txn.commit();
             LOG.info("User deleted: " + (data.targetUsername == null ? data.username : data.targetUsername));
             return Response.ok("{}").build();
