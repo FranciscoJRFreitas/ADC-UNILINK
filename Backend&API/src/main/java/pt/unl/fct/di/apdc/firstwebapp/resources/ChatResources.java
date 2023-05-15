dc97e1a1c61a684ea28a440c963a491048908435
jmamorim <103007274+jmamorim@users.noreply.github.com>
2023-05-15 15:21:35 +0100

    added group

---
+++ Backend&API/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ChatResources.java
@@ -0,0 +1,76 @@
+package pt.unl.fct.di.apdc.firstwebapp.resources;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.gson.Gson;
+import pt.unl.fct.di.apdc.firstwebapp.util.*;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+@Path("/chat")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class ChatResources {
+
+    private final Gson g = new Gson();
+    private static final Logger LOG = Logger.getLogger(ChatResources.class.getName());
+
+    public ChatResources() {
+    }
+
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createGroup(Group group) throws IOException{
+
+        initialize();
+
+        DatabaseReference chatsRef = FirebaseDatabase.getInstance().getReference("chats");
+        DatabaseReference newChatRef = chatsRef.push(); // Generate a unique ID for the new chat
+
+        // Set the data for the new chat
+        newChatRef.child("groupID").setValueAsync(newChatRef.getKey());
+        newChatRef.child("DisplayName").setValueAsync(group.DisplayName);
+        newChatRef.child("description").setValueAsync(group.description);
+
+        DatabaseReference membersRef = FirebaseDatabase.getInstance().getReference("members").child(newChatRef.getKey());
+        //when creating a group the creater becomes the admin
+        membersRef.child(group.adminID).setValueAsync(true); // Set the creator as a member
+
+        DatabaseReference messagesRef = FirebaseDatabase.getInstance().getReference("messages").child(newChatRef.getKey());
+        DatabaseReference newMessageRef = messagesRef.push(); // Generate a unique ID for the new message
+
+        // Set the data for the new message
+        //when the group is created put a welcome message in the group
+        newMessageRef.child("name").setValueAsync(group.adminID);
+        newMessageRef.child("message").setValueAsync("Welcome to " + group.DisplayName + "!");
+        newMessageRef.child("timestamp").setValueAsync(System.currentTimeMillis());
+
+        return Response.ok("{}").build();
+    }
+
+    public void initialize() throws IOException {
+
+        FileInputStream serviceAccount =
+                new FileInputStream("java/pt/unl/fct/di/apdc/firstwebapp/util/unilink23-firebase-adminsdk-a3nn3-d8beef1a33.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://unilink23-default-rtdb.europe-west1.firebasedatabase.app")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
