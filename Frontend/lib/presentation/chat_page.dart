cf4a26fe5f27737e07d9fb0761069ebe2c432b7f
Francisco Freitas <fj.freitas@campus.fct.unl.pt>
2023-06-16 16:59:05 +0100

    chat color create groups changed

--- Frontend/lib/presentation/chat_page.dart
+++ Frontend/lib/presentation/chat_page.dart
@@ -187,6 +187,7 @@ class _ChatPageState extends State<ChatPage> {
         builder: (context) {
           return StatefulBuilder(builder: ((context, setState) {
             return AlertDialog(
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
               title: const Text(
                 "Create a group",
                 textAlign: TextAlign.left,
@@ -245,33 +246,38 @@ class _ChatPageState extends State<ChatPage> {
             }*/
 
   noGroupWidget() {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          GestureDetector(
-            onTap: () {
-              popUpDialog(context);
-            },
-            child: Icon(
-              Icons.add_circle,
-              color: Colors.grey[700],
-              size: 75,
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: GestureDetector(
+                onTap: () {
+                  popUpDialog(context);
+                },
+                child: Icon(
+                  Icons.add_circle,
+                  color: Colors.grey[700],
+                  size: 75,
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "You've not joined any groups, tap on the add icon to create a group or also search from top search button.",
-            textAlign: TextAlign.center,
-          )
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "You've not joined any groups, tap on the add icon to create a group or also search from top search button.",
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
       ),
     );
-  }
+}
 
   void getUsername() async {
     username = await cacheFactory.get('users', 'username');
