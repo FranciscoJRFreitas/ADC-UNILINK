c05bea02cb7dad16f3250caa490bb05e8b65a98e
Martin <m.magdalinchev@campus.fct.unl.pt>
2023-07-02 17:06:42 +0100

    event backend functionalities added

--- Backend&API/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/EventData.java
+++ Backend&API/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/EventData.java
@@ -7,15 +7,17 @@ public class EventData {
     public String description;
     public String startTime;
     public String endTime;
+    public String groupID;
 
     public EventData() {}
 
-    public EventData(String creator, String title, String description, String startTime, String endTime){
+    public EventData(String creator, String title, String description, String startTime, String endTime, String groupID){
         this.creator = creator;
         this.title = title;
         this.description = description;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.groupID = groupID;
     }
 
 }
