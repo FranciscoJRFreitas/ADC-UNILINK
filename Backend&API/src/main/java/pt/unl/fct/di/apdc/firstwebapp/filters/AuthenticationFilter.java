1cc69e974f7e23f3d272ad00c8fd7c0ef39297fe
Francisco Freitas <fj.freitas@campus.fct.unl.pt>
2023-07-15 03:55:14 +0100

    Auth token bug fix

--- Backend&API/src/main/java/pt/unl/fct/di/apdc/firstwebapp/filters/AuthenticationFilter.java
+++ Backend&API/src/main/java/pt/unl/fct/di/apdc/firstwebapp/filters/AuthenticationFilter.java
@@ -7,23 +7,39 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 import static pt.unl.fct.di.apdc.firstwebapp.util.ProjectConfig.datastoreService;
 import static pt.unl.fct.di.apdc.firstwebapp.util.ProjectConfig.g;
 
 public class AuthenticationFilter implements Filter {
+
+    private static final List<String> ALLOWED_PATHS = List.of("/rest/login", "/rest/register", "/rest/recoverPwd", "/rest/activate", "/rest/chat/join");
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String requestURI = httpRequest.getRequestURI();
-        if (requestURI.endsWith("/rest/login/")) {
+        String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length()).replaceAll("/+$", "");
+
+        boolean allowedPath = false;
+        for (String p : ALLOWED_PATHS) {
+            if (path.startsWith(p)) {
+                allowedPath = true;
+                break;
+            }
+        }
+
+        if (allowedPath) {
             chain.doFilter(request, response);
             return;
         }
+
         String authTokenHeader = httpRequest.getHeader("Authorization");
         if (authTokenHeader == null || !authTokenHeader.startsWith("Bearer ")) {
             ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization header must be provided");
